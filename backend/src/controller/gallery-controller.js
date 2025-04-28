09f3a4e7938fdac652d0ae1355da97b2de824218
flaviosenne <flaviosenne123@gmail.com>
2025-04-28 16:04:09 -0300

    ajustar objeto de imagem na listagem, devolvendo a url para o front ter acesso a renderização

--- backend/src/controller/gallery-controller.js
+++ backend/src/controller/gallery-controller.js
@@ -1,10 +1,20 @@
 import galleryService from '../service/gallery-service.js'
 export default {
     list(req, res) {
-        res.status(200).json(galleryService.listFiles())
+        const files = galleryService.listFiles(req)
+        const host = `${req.protocol}://${req.get('host')}`;
+
+        const resp = files.map((file) => ({
+            name: file,
+            url: `${host}/uploads/${file}`
+        }));
+        res.status(200).json(resp)
     },
     findByTitle(req, res) {
-        res.status(200).json(galleryService.findFileByName(req.params.name))
+        const file = galleryService.findFileByName(req.params.name)
+        const host = `${req.protocol}://${req.get('host')}`;
+        const resp = !file ? null : { name: file, url: `${host}/uploads/${file}` }
+        res.status(200).json(resp)
     },
     create(req, res) {
         console.log(req.file)
--- backend/src/middleware/index.js
+++ backend/src/middleware/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import { route } from './routes.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const middleware = express()
+
+// Torna a pasta 'uploads' acessível publicamente
+middleware.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
 
 middleware.use(express.json())
 middleware.use(route)
